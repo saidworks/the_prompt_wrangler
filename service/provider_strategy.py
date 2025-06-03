79b2bb49163cb2cb742c9a144140af0edcf468d0
Said Zitouni <zitouni.sd@gmail.com>
2025-06-02 23:13:04 -0400

    feature/pw004 formatting

--- service/provider_strategy.py
+++ service/provider_strategy.py
@@ -11,16 +11,28 @@ class ProviderStrategy:
     def __init__(
         self,
         provider_name: str,
-        top_p, top_k,
+        top_p,
+        top_k,
         model_reference: str = DEFAULT_MODEL,
         temperature: float = 0.7,
-        max_tokens: int = 512, 
+        max_tokens: int = 512,
     ) -> None:
         self.log = get_logger(type(self).__name__)
         if provider_name == LLM_PROVIDERS.OLLAMA.name:
-            self.provider = OllamaModelService(model_reference, temperature, max_tokens, top_p, top_k,)
+            self.provider = OllamaModelService(
+                model_reference,
+                temperature,
+                max_tokens,
+                top_p,
+                top_k,
+            )
         elif provider_name == LLM_PROVIDERS.AZURE.name:
-            self.provider = AzureOpenAIModelService(temperature, max_tokens, top_p, top_k,)
+            self.provider = AzureOpenAIModelService(
+                temperature,
+                max_tokens,
+                top_p,
+                top_k,
+            )
 
         else:
             raise ValueError("Invalid provider name")
